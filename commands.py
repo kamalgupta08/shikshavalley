a0d15e479b38a2c545a35b533834a6ca21af8347
kamalgupta08 <48328026+kamalgupta08@users.noreply.github.com>
2023-02-05 10:46:23 +0530

    Delete commands.py

--- commands.py
+++
@@ -1,9 +0,0 @@
-import click
-from flask.cli import with_appcontext
-from .extensions import db
-from .models import User, ClassTypes, StudentDetails, StudentFeesDetails
-
-@click.command(name='create_tables')
-@with_appcontext
-def create_tables():
-    db.create_all()
